f33da1d39969c99724378983cb45b1b7de54975a
Luke OSBORNE <X00205719@myTUDublin.ie>
2024-03-30 23:10:58 +0000

    fix: correct config

--- backend.tf
+++ backend.tf
@@ -1,7 +1,19 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
   backend "azurerm" {
     storage_account_name = "terraformlo"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  use_oidc = true
-  features {}
-}
-
 resource "azurerm_resource_group" "kubernetes" {
   name     = "kubernetes"
   location = "West Europe"
