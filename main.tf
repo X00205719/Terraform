636a567cea3af172d26c7b87b26e429eecfc723c
Luke OSBORNE <X00205719@myTUDublin.ie>
2024-03-30 23:50:28 +0000

    fix: test version

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.5.7"
+  backend "azurerm" {
+    resource_group_name  = "Terraform"
+    storage_account_name = "terraformlo"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "kubernetes" {
   name     = "kubernetes"
   location = "West Europe"
