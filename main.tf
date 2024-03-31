13e827f9a92231a26fac45a9fb818fe56a7358bb
Luke OSBORNE <X00205719@myTUDublin.ie>
2024-03-31 00:04:56 +0000

    feat: resource group

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
 }
 
-# resource "azurerm_resource_group" "kubernetes" {
-#   name     = "kubernetes"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "kubernetes" {
+  name     = "kubernetes"
+  location = "West Europe"
+}
